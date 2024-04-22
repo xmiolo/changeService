fc657225480405b5af3da49383aeb1206c94e8bb
xmiolo <ggrbedin@gmail.com>
2024-04-22 16:57:53 -0300

    Initial commit

---
+++ src/main/java/com/adp/changeService/ChangeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.adp.changeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adp/changeService/ChangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adp.changeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
